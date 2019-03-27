13d734f160af46df1d5c7b4797de9bc0622ec9fd
fenno <fenno@ynformed.nl>
2019-03-27 14:24:46 +0100

    Update setup.py and setup.cfg
    Summary:
    The changes:
    
    setup.cfg now also has a pycodestyle section. See https://github.com/PyCQA/pycodestyle/issues/466 for the reason why (thanks guido!)
    
    setup.py now calls find_packages. This is neccecary if your package folder contains subfolders. for example 'sam.preprocessing' is a seperate 'package' for the purpose of the packages option. So we now get everything instead of just the root dofolder.
    
    setup.py now has a long_description as well. Not sure when this is ever relevant, butwhy not.
    
    Test Plan: have the same with SAM, and it works there
    
    Reviewers: Sebastiaan
    
    Reviewed By: Sebastiaan
    
    Differential Revision: https://dev.ynformed.nl/D615

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='{{cookiecutter.pkg_name}}',
       version='{{cookiecutter.version}}',
       description='{{cookiecutter.project_short_description}}',
+      long_description=open('README.md').read()),
       url='',
       author='{{cookiecutter.full_name}}',
       author_email='{{cookiecutter.email}}',
       license='',
-      packages=['{{cookiecutter.pkg_name}}'],
+      packages=find_packages(),
       zip_safe=False,
       install_requires=[]
       )
