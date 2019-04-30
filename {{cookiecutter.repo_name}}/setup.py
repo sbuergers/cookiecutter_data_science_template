80d543e8218702c33125d9008f85f9414f7208fa
Arjan Bontsema <Arjan.Bontsema@hiemstraendevries.nl>
2019-04-30 15:53:50 +0200

    Fix syntax error in setup.py
    Test Plan: -
    
    Reviewers: fenno
    
    Reviewed By: fenno
    
    Differential Revision: https://dev.ynformed.nl/D723

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='{{cookiecutter.pkg_name}}',
       version='{{cookiecutter.version}}',
       description='{{cookiecutter.project_short_description}}',
-      long_description=open('README.md').read()),
+      long_description=open('README.md').read(),
       url='',
       author='{{cookiecutter.full_name}}',
       author_email='{{cookiecutter.email}}',
